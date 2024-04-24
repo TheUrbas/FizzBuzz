18a0f5287dbc8ae91bc8a9aa947b80465b17b322
TheUrbas <macieju0123@gmail.com>
2024-04-24 15:39:52 +0100

    Instantiate a rulesapplicator

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using FizzBuzz.Applicator;
+using FizzBuzz.Rules;
 
-Console.WriteLine("Hello, World!");
+var fizzRule = new FizzRule();
+var buzzRule = new BuzzRule();
+
+var applicator = new RulesApplicator(new List<IRule> { fizzRule, buzzRule });
+
+var numbers = Enumerable.Range(1,100).ToList();
+
+foreach (var result in applicator.ApplyRulesAndReturnList(numbers))
+{
+    Console.WriteLine(result);
+}

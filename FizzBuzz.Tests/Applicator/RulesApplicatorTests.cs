2a7e37e5af8f184591dfcc40de05d0cf39aed015
TheUrbas <macieju0123@gmail.com>
2024-04-24 15:29:48 +0100

    Add failing rule applicator unit tests

--- FizzBuzz.Tests/Applicator/RulesApplicatorTests.cs
+++ FizzBuzz.Tests/Applicator/RulesApplicatorTests.cs
@@ -1,6 +1,39 @@
+using FizzBuzz.Applicator;
+using FizzBuzz.Rules;
+using Moq;
+
 namespace FizzBuzz.Tests.Applicator;
 
 public class RulesApplicatorTests
 {
+
+    [Fact]
+    public void ApplyRulesAndReturnLists_GivenFalseRule_ReturnsUnchangedListOfString()
+    {
+        var mockRule = new Mock<IRule>();
+        mockRule.Setup(rule => rule.isRuleValid(It.IsAny<int>())).Returns(false);
+        
+        var applicator = new RulesApplicator(new List<IRule> { mockRule.Object });
+        var inputList = new List<int> { 1, 2, 3 };
+
+        var result = applicator.ApplyRulesAndReturnLists(inputList);
+        
+        Assert.Equal(inputList, result);
+    }
     
+    [Fact]
+    public void ApplyRulesAndReturnLists_GivenTrueRule_ReturnsTransformedListOfString()
+    {
+        var mockRule = new Mock<IRule>();
+        mockRule.Setup(rule => rule.isRuleValid(It.IsAny<int>())).Returns(true);
+        mockRule.Setup(rule => rule.applyTransformation(It.IsAny<int>())).Returns("test");
+        
+        var applicator = new RulesApplicator(new List<IRule> { mockRule.Object });
+        var inputList = new List<int> { 1, 2, 3 };
+        var expectedOutputList = new List<string> { "test", "test", "test" };
+
+        var result = applicator.ApplyRulesAndReturnLists(inputList);
+        
+        Assert.Equal(expectedOutputList, result);
+    }
 }

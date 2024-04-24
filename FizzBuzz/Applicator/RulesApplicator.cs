c327028698d33075d4e597cafcde4c44ca9ff386
TheUrbas <macieju0123@gmail.com>
2024-04-24 15:33:14 +0100

    Add rule applicator methods

--- FizzBuzz/Applicator/RulesApplicator.cs
+++ FizzBuzz/Applicator/RulesApplicator.cs
@@ -5,4 +5,15 @@ namespace FizzBuzz.Applicator;
 public class RulesApplicator(IEnumerable<IRule> rules)
 {
     public IEnumerable<IRule> _rules = rules;
+
+    public IEnumerable<string> ApplyRulesAndReturnList(IEnumerable<int> numbers)
+    {
+        return numbers.Select(GetRulesTransformation);
+    }
+
+    private string GetRulesTransformation(int number)
+    {
+        var transformedString = string.Join("", _rules.Select(rule => rule.isRuleValid(number) ? rule.applyTransformation(number) : ""));
+        return string.IsNullOrEmpty(transformedString) ? number.ToString() : transformedString;
+    }
 }
